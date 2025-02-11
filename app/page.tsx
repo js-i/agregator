b4fd7730314a5afe059ed91bd4f60d95ab415410
Georgii <menshikov.georgy@gmail.com>
2025-02-11 15:17:11 +0700

    search3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import  Counter from "./reducer/page";
+// import  Counter from "./reducer/page";
 
 export default function Home() {
 
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>основной компонент</h1>
-        <Counter counterId="first"/>
+        {/* <Counter counterId="first"/> */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         
