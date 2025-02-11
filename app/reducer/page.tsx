428ef6ec0fa63ed5088992a28ad60484713a4947
Georgii <menshikov.georgy@gmail.com>
2025-02-11 18:05:16 +0700

    delete reducer2

--- app/reducer/page.tsx
+++ app/reducer/page.tsx
@@ -1,35 +1,35 @@
-"use client"
-import { useEffect, useReducer } from "react";
-import { CounterId, DecrementAction, IncrementAction, store } from "../lib/store";
+// "use client"
+// import { useEffect, useReducer } from "react";
+// import { CounterId, DecrementAction, IncrementAction, store } from "../lib/store";
 
 
-export default function Counter({ counterId } : { counterId: CounterId}) {    
-    console.log(counterId)
-    const [, forceUpdate] = useReducer((x) => x + 1, 0);
-    useEffect(() => {
-        const unsubscribe = store.subscribe(() => {
-            forceUpdate()
-        })
-        return unsubscribe
-    }, [])
+// export default function Counter({ counterId } : { counterId: CounterId }) {    
+//     console.log(counterId)
+//     const [, forceUpdate] = useReducer((x) => x + 1, 0);
+//     useEffect(() => {
+//         const unsubscribe = store.subscribe(() => {
+//             forceUpdate()
+//         })
+//         return unsubscribe
+//     }, [])
 
-    return (
-        <>
-            <div className="text-xl bg-slate-600 text-sky-500 px-6">counter {store.getState().counters[counterId]?.counter}</div>
-            <button 
-                className='border-2 px-4 rounded' 
-                onClick={() => {store.dispatch({type: 'increment', payload: {counterId: counterId}} satisfies IncrementAction)}}
-                >
-                increase
-            </button>
-            <button 
-                className='border-2 px-4 rounded'
-                onClick={() => {store.dispatch({type: 'decrement', payload: {counterId: counterId}} satisfies DecrementAction)}}
-                >
-                decrease
-            </button>
+//     return (
+//         <>
+//             <div className="text-xl bg-slate-600 text-sky-500 px-6">counter {store.getState().counters[counterId]?.counter}</div>
+//             <button 
+//                 className='border-2 px-4 rounded' 
+//                 onClick={() => {store.dispatch({type: 'increment', payload: {counterId: counterId}} satisfies IncrementAction)}}
+//                 >
+//                 increase
+//             </button>
+//             <button 
+//                 className='border-2 px-4 rounded'
+//                 onClick={() => {store.dispatch({type: 'decrement', payload: {counterId: counterId}} satisfies DecrementAction)}}
+//                 >
+//                 decrease
+//             </button>
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
