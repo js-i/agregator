7a5cfd53e42584600c5d8b6339218e67ee38e631
Georgii <menshikov.georgy@gmail.com>
2025-02-15 11:03:16 +0700

    suspence

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useSearchParams, usePathname, useRouter } from "next/navigation"
 import { useDebouncedCallback } from 'use-debounce'
-
+import { Suspense } from "react"
 export default function Search({ placeholder='Search... ' }: { placeholder: string } ){
     const searchParams = useSearchParams()
     const pathname = usePathname()
@@ -19,11 +19,13 @@ export default function Search({ placeholder='Search... ' }: { placeholder: stri
     }, 300)
 
     return (
-        <input type="text" 
-            onChange={(e) => handleChange(e.target.value)} 
-            placeholder={placeholder} 
-            className="border p-3 rounded-lg w-1/3 shadow-sm focus:outline-none focus:ring-2 focus:ring-[#b29889]" 
-            defaultValue={searchParams.get('query')?.toString()}
-        />
+        <Suspense>
+            <input type="text" 
+                onChange={(e) => handleChange(e.target.value)} 
+                placeholder={placeholder} 
+                className="border p-3 rounded-lg w-1/3 shadow-sm focus:outline-none focus:ring-2 focus:ring-[#b29889]" 
+                defaultValue={searchParams.get('query')?.toString()}
+            />
+        </Suspense>
     )
 }
