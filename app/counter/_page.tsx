5be1fd05ba63e0aeadfeb8e36aab989a291600d0
Georgii <menshikov.georgy@gmail.com>
2025-02-26 17:23:47 +0700

    start adding redux2

---
+++ app/counter/_page.tsx
@@ -0,0 +1,9 @@
+// import Counter from '@/app/components/modules/counters/counters'
+// import UsersList from '../components/modules/users/users-list'
+
+// export default function CounterPage() {
+//    return <div>
+//       <Counter counterId='first'/>
+//       <UsersList/>
+//       </div>
+// }
--- app/counter/page.tsx
+++
@@ -1,9 +0,0 @@
-import Counter from '@/app/components/modules/counters/counters'
-import UsersList from '../components/modules/users/users-list'
-
-export default function CounterPage() {
-   return <div>
-      <Counter counterId='first'/>
-      <UsersList/>
-      </div>
-}
